f0a5bcf01997941c8847e4837e4a3855b0c1ebc6
Daniel gagnon <daniel.r.gagnon@outlook.com>
2024-09-24 14:53:09 -0500

    added a skeleton screen for loading

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Skeleton screen
+setTimeout(() => {
+    document.querySelectorAll('.skeleton').forEach(el => el.style.display = 'none');
+    document.querySelectorAll('.hidden').forEach(el => el.style.display = 'block');
+}, 3000)
+
 function checkScroll () {
     const navbar = document.getElementById('nav-bar');
     const logo = document.getElementById('logo');
