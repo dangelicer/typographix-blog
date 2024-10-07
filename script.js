62299643068afaa8f9879c6aa36ce2eeabbacb9b
Daniel gagnon <daniel.r.gagnon@outlook.com>
2024-10-07 00:39:16 -0500

    added scss

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Skeleton screen
+// Skeleton screen ---------------------------------------------------
 setTimeout(() => {
     document.querySelectorAll('.skeleton').forEach(el => el.style.display = 'none');
     document.querySelectorAll('.hidden').forEach(el => el.style.display = 'block');
@@ -27,3 +27,50 @@ function checkScroll () {
 }
 
 window.addEventListener('scroll', checkScroll);
+
+//Dark & Light Modes -----------------------------------------------------
+const themeSwitcher = document.getElementById('theme-switcher');
+
+// Update Icon
+function updateThemeIcon (isDarkMode) {
+    themeSwitcher.children[0].classList.replace(isDarkMode ? 'fa-sun' : 'fa-moon', isDarkMode ? 'fa-moon' : 'fa-sun');
+}
+
+// Determine if dark mode is preffered
+function prefersDarkMode () {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+// Set theme based on preferance
+function setThemeOnPreference () {
+    const isDarkMode = prefersDarkMode()
+    document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
+    updateThemeIcon(isDarkMode);
+}
+
+function switchTheme () {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+    updateThemeIcon(newTheme === 'dark');
+}
+
+// Check Local Storage
+function initializeTheme () {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme) {
+        document.documentElement.setAttribute('data-theme', savedTheme);
+        updateThemeIcon(savedTheme === 'dark');
+    } else {
+        setThemeOnPreference();
+    }
+}
+
+// Listen for theme changes
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setThemeOnPreference)
+themeSwitcher.addEventListener('click', switchTheme);
+
+// Initialize theme when script loads
+initializeTheme();
